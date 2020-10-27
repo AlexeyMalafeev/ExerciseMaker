691fcd32ebade137f0db669470c27e355192ea02
AlexeyMalafeev <amalafeev@yandex.ru>
2020-10-27 16:06:37 +0300

    open_cloze.py

--- ex_maker.py
+++ ex_maker.py
@@ -351,41 +351,6 @@ def quit():
     sys.exit()
 
 
-class OpenClozeMaker(GapMaker):
-    def __init__(self, text_obj, rem_words, submode_name):
-        self.rem_words = rem_words
-        self.submode_name = submode_name
-        GapMaker.__init__(self, text_obj)
-
-    def check_condition(self, i, w):
-        orig_w = self.orig_no_punc[i]
-        return w in self.rem_words and (orig_w[1:].islower() or len(w) == 1)
-
-    def make_gap(self, i, count):
-        lpunc, rpunc = self.punc[i]
-        self.ex_text[i] = '({}){}{}{}'.format(count, lpunc, GAP, rpunc)
-        self.ans_key.append('({}) {}'.format(count, self.orig_no_punc[i]))
-
-    def set_instr(self):
-        self.instr = '''Fill the gaps with suitable words, such as articles, prepositions, conjunctions, pronouns and \
-auxiliaries:'''
-
-    def set_tag(self):
-        self.tag = '[open {}{}]'.format(self.submode_name, get_gap_info(self.max_num_gaps))
-
-
-class OrderedOpenClozeMaker(OpenClozeMaker):
-    def __init__(self, text_obj, rem_words, order, submode_name):
-        """order is a list of words, from less to more relevant"""
-        self.rem_words = rem_words
-        self.order = order
-        self.submode_name = submode_name
-        GapMaker.__init__(self, text_obj)
-
-    def get_indices_from_dict(self, num_w):
-        return ind_from_dict_ordered(self.indices, self.order, num_w, prox=self.gap_distance)
-
-
 class PrepositionsMWM(MissWordsMaker):
     words_after_to = set('''a an the some any no this that these those me you him her it its itself us them my mine
     myself your yours yourself his himself hers herself our ours ourselves their theirs themselves what whatever who
