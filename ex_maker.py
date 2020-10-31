b40b830d8aaaf46f6fab6c5c66e2441194486ec9
AlexeyMalafeev <amalafeev@yandex.ru>
2020-10-31 16:32:02 +0300

    complexity.py

--- ex_maker.py
+++ ex_maker.py
@@ -85,10 +85,6 @@ Return (left_punc_string, right_punc_string):
     return get_word_and_punc(word)[1:]
 
 
-def get_ratio(val_a, val_b):
-    return round(val_a / val_b, 3)
-
-
 def get_word_and_punc(word):
     """
 Return (word_without_punc, left_punc_string, right_punc_string):
@@ -246,31 +242,6 @@ def num_to_letter(n):
     return '{}{}'.format(letters[b], a if a > 0 else '')
 
 
-def rank(rdicts_dict):
-    d = rdicts_dict
-    ranking = {k: 0 for k in d}
-    for m in COMPL_MEASURES:
-        m_ref = '![{}]'.format(m)
-        values = set()
-        for k in d:
-            values.add(d[k][m_ref])
-        t_ranks = {v: i + 1 for i, v in enumerate(sorted(values))}
-        for k in ranking:
-            ranking[k] += t_ranks[d[k][m_ref]]
-    r_order = sorted(ranking, key=ranking.get)
-    mlen = max((len(k) for k in ranking)) + len(str(len(ranking))) + 2
-    file_dir = jp(OUTPUT_DIR, '![compl ranking].txt')
-    with open(file_dir, 'w') as f:
-        for i, k in enumerate(r_order):
-            f.write('{:<{}}  {}\n\n'.format('{}. {}'.format(i + 1, k), mlen, ranking[k]))
-
-
-def register_measures(text_obj, rdict):
-    measures = COMPL_MEASURES
-    for m in measures:
-        rdict['![{}]'.format(m)] = getattr(text_obj, m)
-
-
 def remove_punc(word):
     return get_word_and_punc(word)[0]
 
@@ -291,20 +262,6 @@ def safely_remove_word(i, end, orig_no_punc, ex_text, punc):
     ex_text[i] = ''
 
 
-def save_all_stats(all_stats):
-    file_dir = jp(OUTPUT_DIR, '[all ratios].txt')
-    d = all_stats
-    keys = sorted(d)
-    subkeys = sorted(d[keys[0]])
-    with open(file_dir, 'w') as f:
-        for key in keys:
-            ratios = ', '.join(['{}. {}'.format(k, d[key][k]) for k in subkeys])
-            f.write('{}: {}\n\n'.format(key, ratios))
-    subkeys.remove('_title_')
-    for sk in subkeys:
-        save_one_stat(all_stats, sk)
-
-
 def save_ex(text_output, file_id, mode_id):
     ##        log.append('{}\n{}\n\n'.format(file_id, text_output))
     text_output = text_output.replace(NEW_LINE_MARKER, '\n\n')
@@ -312,24 +269,6 @@ def save_ex(text_output, file_id, mode_id):
         f.write(text_output.split('ANSWERS')[0])
     with open(jp(CWD, 'output', file_id, '{} ({}) (with key).txt'.format(file_id, mode_id)), 'w') as f:
         f.write(text_output)
-
-
-def save_one_stat(rdicts_dict, key):
-    file_dir = jp(OUTPUT_DIR, '{}.txt'.format(key))
-    d = {k: v[key] for k, v in rdicts_dict.items()}
-    mlen = max((len(k) for k in d))
-    keys = sorted(d, key=d.get)
-    with open(file_dir, 'w') as f:
-        for k in keys:
-            f.write('{:<{}}  {}\n\n'.format(k, mlen, d[k]))
-
-
-def save_text_stats(text_stats):
-    keys = sorted(text_stats)
-    file_dir = jp(OUTPUT_DIR, text_stats['_title_'], '[stats].txt')
-    with open(file_dir, 'w') as f:
-        for k in keys:
-            f.write('{:<17}{}\n'.format(k, text_stats[k]))
 
 
 def smart_cap(word):
